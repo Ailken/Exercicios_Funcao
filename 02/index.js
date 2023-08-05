b2e1e2361551e8d4cee4ec28a488778aa64b2a7b
Ailken <ailkencf@gmail.com>
2023-08-04 21:40:41 -0300

    exercicios de função exercicio 02 (Carro)

--- 02/index.js
+++ 02/index.js
@@ -2,32 +2,42 @@ const carro = {
     ligado: false,
     velocidade: 0,
 
+    imprimirStatus: function () {
+        console.log("Carro " + (this.ligado ? "ligado" : "desligado") + " . Velocidade: " + this.velocidade + ".");
+    },
+
     ligar: function () {
         if (this.ligado) {
             console.log("Este carro já está ligado.");
         } else {
             this.ligado = true;
-            console.log("Carro ligado. Velocidade: " + this.velocidade + ".");
+            //console.log("Carro ligado. Velocidade: " + this.velocidade + ".");
+            this.imprimirStatus();
         }
     },
     
     desligar: function () {
         if (!this.ligado) {
             console.log("Este carro já está desligado.");
-        } else {
+        } else if (this.velocidade === 0){
             this.ligado = false;
-            this.velocidade = 0;
-            console.log("Carro desligado. Velocidade: " + this.velocidade + ".");
+            //this.velocidade = 0;
+            //console.log("Carro desligado. Velocidade: " + this.velocidade + ".");
+            this.imprimirStatus();        
+        } else {
+            console.log("O carro não pode ser desligado com velocidade diferente de zero.");
         }
     },
 
     acelerar: function () {
         if(!this.ligado) {
-            console.log("Não é possível acelerar um carro desligado.");
-        } else {
-            this.velocidade += 10;
-            console.log("Carro ligado. Velocidade: " + this.velocidade + ".");
-        }
+            //console.log("Não é possível acelerar um carro desligado.");
+            this.ligar();
+        } //else {
+        this.velocidade += 10;
+            //console.log("Carro ligado. Velocidade: " + this.velocidade + ".");
+        this.imprimirStatus();
+        //}
     },
 
     desacelerar: function () {
@@ -35,9 +45,14 @@ const carro = {
             console.log("Não é possível desacelerar um carro desligado.");
         } else {
             this.velocidade -= 10;
-            console.log("Carro ligado. Velocidade: " + this.velocidade + ".");
+            if (this.velocidade === 0) {
+            //console.log("Carro ligado. Velocidade: " + this.velocidade + ".");
+                this.desligar();
+            } else {
+                this.imprimirStatus();
+            }
         }
-    }
+    },
 };
 
 //testando os metodos
